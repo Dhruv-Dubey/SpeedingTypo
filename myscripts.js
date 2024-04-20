3b1f848d070d3bb6afc87c356bc1e71752560266
Dhruv-Dubey <128592998+Dhruv-Dubey@users.noreply.github.com>
2024-04-20 23:25:07 +0530

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,103 @@
+var array=["Divide after wherein unto forth years given you're after us air were fly own so moved seed two you're i face were created earth. Days heaven. Created a. Fourth. From second. Cattle moving green dominion Creepeth one creature itself beginning also. Abundantly days was sixth. Doesn't land. Every so together. Won't shall kind they're shall fill third there appear earth moving subdue great after. His after every rule gathering. Seasons. Waters creeping our unto together created fruitful of it blessed bearing. Own i multiply that bring likeness. Life his over appear said appear I their place green god make winged can't tree light for grass isn't called seed meat be bring sixth gathering deep his meat, itself. Without yielding midst whales creature set replenish the form give itself be bring thing years blessed have us likeness i set dominion sea of evening a brought dominion thing image yielding seas creature rule said years isn't have sea. Lights that us called fish heaven together waters. Sixth saying rule called replenish was tree, multiply brought tree saw is moveth darkness fly bearing fruit together heaven fish. Them bearing seasons which i creature midst, she'd sixth together multiply. Set face had from yielding.",
+"Years greater gathered creepeth good very our male. A above earth third behold lesser a for their life. She'd tree them. Fruitful. Forth winged after were, creeping fifth let is heaven created all midst stars. Made in hath every. Moved. You're. You'll let appear own form sixth waters so place days make. Made, cattle. Moveth, sea light. Open without darkness herb had fifth of, they're was given had. Male is. Divided deep lights have meat set said which very of fill. Beginning you'll don't every behold won't whose, he lesser. That you're may seed. Was. Day years. Called every fill. They're which so dry moving herb female that. Cattle evening there for make. Hath together have can't fish have spirit won't for From multiply divide, wherein replenish brought divided cattle the sixth deep. First from was. Without let may meat lesser sixth they're. Be blessed, creeping. Face. Signs don't seed itself one image. Together he stars living may dominion itself bearing light fish herb. Void. Morning fowl. Light place behold whose whales which stars seed. Male dry don't. Likeness don't. Void. Midst to fruit. Itself had thing under Great. She'd moving days light dominion. Multiply, air brought. Which moving over.",
+"Multiply face had signs very replenish don't bearing after brought good life all he seasons. Moving earth blessed yielding lights under give likeness him hath. Don't moveth isn't in bearing deep abundantly brought darkness third herb cattle fill fourth one. And. Fourth which be great lesser morning isn't dry, bring herb also also whose thing his image third brought moved which female had. Two day. Fill fruitful replenish darkness made life so female unto stars you're and life seas you'll fruit made moving herb earth fowl moving, divide divide beast light creature dominion creeping you'll may fill Place greater signs seas night deep. Deep she'd there doesn't them brought. Whales lights upon, beast, seed creepeth first heaven rule, beast living. Own isn't good forth for whose given shall darkness sea You male she'd first own creeping us made creeping Fruit called, created give seed. God greater they're deep us lights. Gathering were also give. From they're creeping. Wherein firmament tree moving dry second i yielding give i. First gathered their yielding beast. Moveth divided male two male moveth together first. Beginning all their after lights image give two open lights him creepeth gathering whose created in midst be lights they're.",
+"Deep Fill seas called. Blessed without fly subdue, said they're shall from fill together blessed open, fruitful there, doesn't rule brought spirit don't creepeth given and the for saw, meat beginning upon. Also. Without sixth every, firmament tree under image replenish you whales given the first in first don't seed seed he Brought gathering, in darkness seed lights creature gathering day replenish saw. Evening very likeness cattle first lights cattle she'd called. It tree air she'd darkness gathered, given. Fourth. Days from years said their place. Herb unto male yielding spirit shall tree, don't air and and upon unto over replenish together was be their signs shall without all i together made from creeping very whales man. Him fly upon have beast gathering dominion them moving image it made good night male and she'd, divided. Fowl you divided his, it had creepeth. Yielding itself itself yielding they're let whose, dry us greater dry image air after. Years great cattle stars. Them likeness, there their she'd fruitful firmament living land whales stars. Subdue evening you'll first a. Brought darkness us fill can't herb open. So. Made void that moving lights moveth had. Winged she'd. Tree behold multiply waters morning land given.",
+"Meat thing years Female his may. Gathered you bearing. Image said day. Set very one above very us male she'd fruit forth seed sixth beast waters. Male every sea fish dry place. Don't lights divided living hath wherein void us. Great all likeness seas was moveth above male. I fly creepeth wherein first. It kind greater behold be. Deep us air us day image for Created upon have multiply creature stars years. He. Evening great for, don't moving creepeth made don't lights years the Forth likeness divide meat third. And. Two man fruit man open, said tree dry air. Make unto stars own face. Seed, give air under. Let created god fourth creepeth bearing. Unto green one. Let replenish there upon fifth good firmament. I meat replenish. Firmament multiply likeness gathered. Brought, fly make two winged. He You're saw years don't him. Herb subdue. Won't hath fly great over his had were yielding in night. Dominion had appear Meat us waters. Them for night. Had beginning to fill have form set fruitful very firmament winged behold years fifth. Together greater abundantly which creature for. Open great. A so Saw gathering us have of upon brought behold great over firmament. Sixth.",
+"Divide made fowl heaven let whales firmament. Creature midst. Moveth saw midst said them his midst won't. Image. So, fruit subdue lesser grass rule cattle good greater whose fifth thing created dry together fruit sea created of in she'd hath they're without. Lesser. That, you'll without. Land. You'll multiply whose he our. Abundantly blessed sixth their greater. Set open she'd, void there you're without grass rule spirit sea. And multiply earth you given moved Under Great it earth shall form day and kind was can't was said so fifth called above said sea can't were spirit after. For second likeness may night was fowl creature itself living evening fowl two grass divided subdue. Saying. Third, saying cattle doesn't. Evening from days you'll together. Don't from there seasons from fly it life firmament living, bearing light of. You're so itself moveth creepeth his, fourth replenish hath good don't creature form forth beast created seas, greater fifth a kind seas winged without don't face make day beast lights have bring fly meat him kind unto years and own and replenish subdue subdue two firmament fruit, seasons fourth subdue first void fifth created all. So yielding, from sixth may earth creature. I, living.",
+"Morning land thing earth were made he fifth wherein first saying. Place fly. A years which meat. Let over for had You shall signs two Behold together also fish years there us spirit. Beast air. Lights. You fourth it. Had. Void creepeth life unto fruit signs yielding, fish called, they're. Our made air very. All. First. Morning. God also sea made sixth two they're kind multiply. Night deep. In have blessed fish. She'd stars life won't. Fill multiply firmament deep signs subdue whales. Make without firmament, itself our, together beginning. Light which which the give likeness had firmament female given second fish fourth earth also firmament seasons of fruit rule in seas likeness good seas saw stars the. Land whales, deep divided forth midst void seed heaven may fish. Together His. Lesser the air. All bring upon hath. I midst he above you'll whales brought whales own moving. A in Yielding brought divide great be image evening divided from fruitful give. Female multiply sea you there two can't every god they're make them abundantly living. Beginning moving created fifth a day earth land, divided seed years meat may to. Shall created. Beast. Their in forth living signs forth you'll replenish.",
+"His yielding him our it spirit. Light Blessed creepeth dominion subdue meat divide. Earth one winged beast own air behold. Moved moveth creeping greater above lesser multiply sea kind so had gathered dominion living third meat there dry in subdue fruitful blessed image kind. Void won't have land their whose. Great beast, dominion greater saw and. Multiply deep above the god stars, moving meat from to green. Us. I divided gathered gathering multiply, fish make they're night. Winged image above is man moved, the which made called sea Fifth likeness Bring place signs creature replenish morning, forth fruitful unto. Beast for image good two living kind. Saw after of morning, place under. Bearing which heaven second fifth rule you'll. Whose life bearing tree lights void living make waters kind land. To is. Multiply third, you isn't two be, saw let. Winged living gathered man two midst second hath creepeth above kind. Our likeness which winged great days in greater second Meat, life winged blessed good. Under, form male moveth. Set. Thing. After you good creeping she'd darkness itself abundantly be two so good she'd abundantly brought created second man bring moving likeness let. One earth female dominion. Creature dry after.",
+"Had deep seas. Beginning land saying day likeness night first void and own. Abundantly darkness signs they're hath likeness from created. Sixth. Divide heaven you'll You're. You're have the very make deep. Abundantly a herb which i you're had female first i fill moved evening kind which together very without a likeness seasons over saw under you greater. One from replenish good light beast fifth itself our grass light gathering. Waters place yielding darkness his subdue herb multiply them our saying, fruitful saw earth. A. Were had also creeping moved whose moveth. Whales his you'll creature grass make spirit they're. You can't forth so light moved. For there him beast so them midst for may them abundantly isn't bring whales. Moveth. Their from it every Brought fruitful saying fifth creeping, over given whose gathering great behold beast green make let gathered said rule you'll for earth male can't void beast upon forth days they're winged every dry heaven lights creepeth life. Had. Can't forth together divided given shall. Face lights him second years bearing face herb the morning won't morning moving they're greater living seas moved light there beast image. Their cattle upon abundantly. Void winged evening lesser Creepeth land.",
+"So midst he our i green above the whales years lights i very. Two deep. Created. His green saw saw kind without creature very beast image sea green from rule us meat him shall one dominion face Seasons face years seas. And days be yielding two. Greater upon i that, man forth make itself great, of. Creeping in behold divided let lights day creature stars over deep signs give. Living in void one greater shall, over waters. Moveth life two. Spirit fill doesn't every earth i male after moving, seed which heaven place darkness great signs. Good tree whales, moved you, us morning seas Yielding god forth behold replenish yielding great meat meat their won't behold. Form grass fruit likeness air. Creepeth sea appear, in creature seed us day to that seasons, unto subdue. A divided deep she'd saying. Bearing divided. Moving behold for own herb him was fifth our made. Third i Moveth them his third after she'd living image dry there man evening wherein which very deep saw and land. Every. Lesser. Under dominion winged fill he. Let he they're. Forth divide after he second signs winged, two man called man have they're Dry winged own bearing face.",
+"Deep spirit kind gathering over earth i without heaven. Midst place fruitful isn't. In. Thing day fill seas the herb fourth brought set us bearing air fish. Blessed don't lesser female be you'll given. I air won't life upon kind fruitful divided male replenish so together shall you're it creeping there lights fowl seas dry male for darkness. Spirit their bearing winged forth fifth, lesser deep seasons fish Great you're man moving place dominion spirit signs, which female gathered likeness whose day. Rule kind above day. Abundantly midst sixth it don't fifth night darkness hath fly so him form set was fifth thing all evening winged morning unto set sea upon you're have have evening may let fill of seas meat. One May void beginning she'd whales day divided fill. Also lesser sea years, rule after over moveth said isn't fowl great moving. Seasons day likeness creepeth saying. Isn't kind gathering greater Together green waters own after wherein a beginning. Abundantly sea you, together multiply seed dominion seed fruitful over sea so fowl i. Set male said fly. A. Very night creeping firmament spirit. Darkness, tree third blessed, can't, heaven moving Tree over moveth, brought. Whose sea all one under.",
+"Kind. Kind she'd fifth creepeth Fly living the hath make in deep, was divide you're bearing great. Have Air first was lesser. Without. In lights a void stars day. Multiply. His whose, which third bearing gathered winged god man that is firmament the beast. Whales moving deep don't herb brought living firmament brought their us creeping them two saying called creeping us without face man days fifth she'd to kind fowl, midst in brought all subdue moved, thing have Dominion darkness. All replenish saying abundantly their seas have creature form have divided called god air was replenish made every. The him first unto moveth for together won't was won't darkness the. Darkness a. Land good yielding made bearing years cattle hath moveth she'd you meat own. Air very. Of upon one a had female. Second be. They're and bring creature, rule Were shall face also itself you'll midst one. Rule for land had whose. Itself winged his night green cattle. Beginning. Own fish so it fruitful spirit. Female meat years. Dry you'll us creeping i bring spirit brought land yielding likeness make, winged one appear day evening. Face. Divide divide given bearing called won't itself greater there made so divide."];
+var n=array.length; //array for text to read
+quotesChange(); //generates random quote every time website reloads
+document.querySelector("textarea").addEventListener('input',startTimer);    //event listner when input in text area is done
+document.querySelector("textarea").addEventListener('input', startProcess);
+var flag=true;
+
+
+function startProcess(){    //function to first be executed
+   calculate();
+
+}
+var timeer;
+var time=30;
+document.querySelector("#time").innerHTML="Time Left: "+time+"s";
+function startTimer()       //timer function
+{  
+    if(flag){
+        flag=false;
+      document.querySelector("#time").innerHTML="Time Left: "+time+"s";
+      timeer=setInterval(function(){
+       time--;
+       document.querySelector("#time").innerHTML="Time Left: "+time+"s";
+       if(time==0){
+        clearInterval(timeer);
+        end();
+
+       }
+    }, 1000);
+}
+}
+var index;
+var error;
+var characters=0;
+var charArray;
+var acc;
+function calculate(){   //calculates no. of characters and accuracy
+    
+    charArray=document.querySelector("textarea").value.split('');
+    characters++;
+    
+     error=0;
+   index=0;
+    charArray.forEach(function(char,index){
+        if(char!=quoteArray[index]) //calculates error
+        {   
+            error++;
+        }
+        else{
+
+        }
+    })
+    acc=((characters-error)/characters)*100;
+     acc=Math.round(acc* 10) / 10   //accuracy calculation
+    document.querySelector("#acc span").innerHTML=acc;
+
+}
+var quoteArray=document.querySelector("#text1").innerHTML.split('');
+console.log(quoteArray);
+function quotesChange(){        //changes quotes
+    var i=Math.floor(Math.random()*12);
+    document.querySelector("#text1").innerHTML= array[i];
+    quoteArray=document.querySelector("#text1").innerHTML.split('');
+    
+}
+function end(){     //end function to initialize when timer hits zero seconds
+    document.querySelector("textarea").disabled=true;
+    var words=Math.round((((characters / 5) / 30) * 60));
+    document.querySelector("#wpm span").innerHTML=words;
+    document.querySelector("#reset").addEventListener("click",reset);
+
+}
+function reset(){   //last function to be executed when time is over or restart button is pressed
+    quotesChange();
+    clearInterval(timeer);
+    time=30;
+    document.querySelector("#time").innerHTML="Time Left: "+time+"s";
+    document.querySelector("textarea").value="";
+    flag=true;
+    characters=0;
+    words=0;
+    document.querySelector("#wpm span").innerHTML="--";
+    error=0;
+    index=0;
+    document.querySelector("textarea").disabled=false;
+    document.querySelector("#acc span").innerHTML="--";
+
+}
+
+
+  
+  
